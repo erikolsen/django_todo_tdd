b2d9f4dc9ab6bd4c05390d4f1b89f0bf13682490
Erik Olsen <erik.s.olsen@gmail.com>
2019-09-17 10:31:43 -0500

    Refactor to use list view

--- lists/views.py
+++ lists/views.py
@@ -3,8 +3,11 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/only-list/')
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lists.views import home_page
+from lists.views import home_page, view_list
 
 urlpatterns = [
     path('', home_page),
+    path('lists/only-list/', view_list),
 ]
